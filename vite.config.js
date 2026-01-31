f9f21db2b92204241a9cab5ac53188d409a221fc
Jahidul Islam <ionzahid0987@gmail.com>
2026-01-31 18:47:58 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,47 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import { SvelteKitPWA } from 'vite-plugin-pwa';
 
+export default defineConfig({
+    plugins: [
+        sveltekit(),
+        SvelteKitPWA({
+            strategies: 'generateSW',
+            manifest: { 
+                // We use a placeholder here, but we will override this 
+                // by providing a static manifest.json for better control.
+                name: 'Lumina Manager', short_name: 'Lumina' 
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'google-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365 // < 1 year
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
+                    },
+                    {
+                        urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'cdn-cache',
+                            expiration: {
+                                maxEntries: 20,
+                                maxAgeSeconds: 60 * 60 * 24 * 30 // < 30 days
+                            }
+                        }
+                    }
+                ]
+            }
+        })
+    ]
+});
